ffbbbfe9e3074957468ff4cf50d2701689005bcb
Kelly Mullaney <mullaney.kelly.k@gmail.com>
2022-09-20 15:45:59 -0400

    radio

--- app.py
+++ app.py
@@ -10,12 +10,20 @@ st.write(("_italic text_"))
 
 df = pd.read_csv("data/Austin_Animal_Center_intakes-091922.csv")
 
-type_counts = df['Animal Type'].value_counts(normalize=True)
+intake = st.radio(label="What intake type?", options=df['Intake Type'].unique())
 
+segment_df = df.loc[df['Intake Type'] == intake]
+
+type_counts = segment_df['Animal Type'].value_counts(normalize=True)
+
+st.dataframe(sement_df)
+
+# Version 1
 fig, ax = plt.subplots()
 
 ax.bar(x=type_counts.index, height=type_counts)
 
-st.dataframe(type_counts)
-
 st.pyplot(fig)
+
+# Version 2
+st.bar_chart(type_counts)
