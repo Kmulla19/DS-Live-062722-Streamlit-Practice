13ebd206b4d9d93e62e4aaa4e8d788f4e66b9558
Kelly Mullaney <mullaney.kelly.k@gmail.com>
2022-09-20 15:36:27 -0400

    animal type

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import pandas as pd
 
-st.write("# HELLO WORLD")
+st.write("# SUP")
+
+st.write("## Level Two Header")
+
+st.write(("_italic text_"))
+
+df = pd.read_csv("data/Austin_Animal_Center_intakes-091922.csv")
+
+type_counts = df['Animal Type'].value_counts(normalize=True)
+
+st.datafram(type_counts)
