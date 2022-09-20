9d7b9ae2fec811bb1e69398ab2670e0868d2df4a
Kelly Mullaney <mullaney.kelly.k@gmail.com>
2022-09-20 16:12:22 -0400

    buttons

--- app.py
+++ app.py
@@ -10,4 +10,27 @@ col_names = ['Color_black', 'Fixed', 'Type_Cat', 'Type_Dog', 'Intake Condition_N
 
 color = st.checkbox(label="Is the animal black?")
 
-st.write(color)
+fixed = st.checkbox(label="Is the animal fixed?")
+
+type = st.radio(label='What type of animal?', options=['Dog', 'Cat', 'Other'])
+
+if type == 'Dog':
+    dog_type = True
+    cat_type = False
+elif type == 'Cat':
+    cat_type = True
+    dog_type = False
+else:
+    cat_type = False
+    dog_type = False
+
+condition = st.checkbox(label="Is the intake condition not normal")
+
+female = st.checkbox(label="Is the animal female?")
+
+young = st.checkbox(label="Is the animal young?")
+
+input_row = [color, fixed, cat_type, dog_type, condition, female, young]
+
+input = pd.DataFrame(dict(zip(used_cols, input_row)), index=[0])
+st.write(input)
